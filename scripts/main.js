0cf65df64fc3456f56067da82c6bfe271a064cdb
Taylor Foster <tagfoster90@gmail.com>
2015-01-23 16:12:57 -0500

    catalan opening... the hard way

--- scripts/main.js
+++ scripts/main.js
@@ -13,11 +13,49 @@ function print(board) {
   console.log(board.join('\n') + '\n\n');
  }
 
-function move(){
-	board[4][4] = board[6][4];
-	board[6][4] = ' ';
+function movePawnW(){
+	board[4][3] = board[6][3];
+	board[6][3] = ' ';
 }
-move();
-
-
+movePawnW();
+function movePawnW2() {
+	board[4][2] = board[6][2];
+	board[6][2] = ' ';
+}
+movePawnW2();
+function moveKnightW(){
+	board[5][5] = board[7][6];
+	board[7][6] = ' ';
+}
+moveKnightW();
+function movePawnW3() {
+	board[5][6] = board[6][6];
+	board[6][6] = ' ';
+}
+movePawnW3();
+function moveBishopW() {
+	board[6][6] = board[7][5];
+	board[7][5] = ' ';
+}
+moveBishopW();
+function movePawnB(){
+	board[3][3] = board[1][3];
+	board[1][3] = ' ';
+}
+movePawnB();
+function movePawnB2() {
+	board[2][4] = board[1][4];
+	board[1][4] = ' ';
+}
+movePawnB2();
+function moveKnightB() {
+	board[2][5] = board[0][6];
+	board[0][6] = ' ';
+}
+moveKnightB();
+function moveBishopB(){
+	board[1][4] = board[0][5];
+	board[0][5] = ' ';
+}
+moveBishopB();
 print(board);
