60f1b7d2c6940f318b379bb7cb60572c99413776
Taylor Foster <tagfoster90@gmail.com>
2015-01-27 15:49:10 -0500

    more gym

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -2,17 +2,20 @@ var events = require('../apis/github/users/TaylorFoster90/events.json');
 var assert = require('assert');
 
 function answer(){
+	var pushEvents = events.filter(function(item){
+		return item.type == "PushEvent";
 
+	});
 	return {
 		'total': events.length,
 		'PushEvent': {
-			'total': 5
+			'total': pushEvents.length
 
 		}
 	}
-
 }
 var theAnswer = answer();
+console.log(theAnswer)
 it('should have events', function(){
 	assert(events);
 });
