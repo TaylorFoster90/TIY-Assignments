e049a8870d77d90cf1a592bf7e7be73fc194de5d
Taylor Foster <tagfoster90@gmail.com>
2015-02-07 14:28:54 -0500

    made the json file but im not able to pull info locally i have to get() from https:

--- pub/js/main.js
+++
@@ -1,9 +0,0 @@
-// $('#contribution a').click(function (e) {
-//   e.preventDefault();
-//   $('#contribution a[href="#contribution"]').tab('show');
-// });
-
-$('#nav-tabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
