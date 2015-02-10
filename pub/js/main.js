3a9c216ebf36faf93bd0d3fafa1eaa2cdede2084
Taylor Foster <tagfoster90@gmail.com>
2015-02-09 23:21:15 -0500

    messing with objects and constructors unrelated to chess. just a little object gymnastics

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,9 @@
+//going to fool around with some constructors non related to chess, just a warm up 
 
+function Person(name){
+	this.name = name; 
+}; 
+
+var taylor = new Person('Taylor');
+
+console.log(taylor instanceof Person);
