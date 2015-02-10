2eb7ced9f64855d71ead39eb55b0e89dfe5232bc
Taylor Foster <tagfoster90@gmail.com>
2015-02-10 15:38:45 -0500

    made a piece creator and a new rook

--- scripts/pieces.js
+++ scripts/pieces.js
@@ -5,5 +5,18 @@ function Piece(color, name, position){
 	this._name = name, 
 	this._position = position
 }
+Piece.prototype = {
+	getPosition: function(){
+		return this._position;
+	},
+	getColor: function(){
+		return this_.color;
+	},
+	getName: function(){
+		return this_.name;
+	}	
+}
 
-console.log(assert.typeOf(Piece, 'Object', 'Piece should be an object'));
+var rook = new Piece('white', 'rook', 'queenside');
+
+console.log(rook);
