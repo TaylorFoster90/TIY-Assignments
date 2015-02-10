cb5e7c78175c9e394306ec302bddca0f26167b47
Taylor Foster <tagfoster90@gmail.com>
2015-02-09 20:17:18 -0500

    we made it @jaredstevick @jgarcia99

--- colorClock/js/main.js
+++ colorClock/js/main.js
@@ -3,17 +3,22 @@ function clockWork(){
 	var h = d.getHours();
 	var m = d.getMinutes();
 	var s = d.getSeconds();
-	var myArray = ['A', 'B', 'C', 'D', 'E', 'F'];
-	function arrayLoop(){ for(i = 0; i < myArray.length; i++){
-		return myArray[i];}
-	}
+	var hhex = Math.floor(h * 255/24).toString(16);
+	var mhex = Math.floor(m * 255/59).toString(16);
+	var shex = Math.floor(s * 255/59).toString(16);
 
-	if(h <= 9){h = arrayLoop() + h};
-	if(m <= 9){m = arrayLoop() + m};
-	if(s <= 9){s = arrayLoop() + s};
-	clockColor = "#" + h + m + s; 
-	$('html').css("background-color", clockColor)
-	$('h1').text(clockColor);
+	if(h <= 9){h = "0" + h};
+	if(m <= 9){m = "0" + m};
+	if(s <= 9){s = "0" + s};
+	if(hhex.length == 1){ hhex = "0" + hhex};
+	if(mhex.length == 1){ hhex = "0" + mhex};
+	if(shex.length == 1){ hhex = "0" + shex};
+
+	realTime = h+":"+m+":"+s;
+	clockColor = "#" + hhex + mhex + shex; 
+	$('html').css("background-color", clockColor);
+	$('#clock').text(clockColor);
+	$('#time').text(realTime);
 	setInterval(clockWork, 1000);
 };
 clockWork();
