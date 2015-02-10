c5167c3a7026596843f75f796819522e0806fdc3
Taylor Foster <tagfoster90@gmail.com>
2015-02-09 19:25:42 -0500

    browser sync fucks my commit flow up

--- colorClock/js/main.js
+++ colorClock/js/main.js
@@ -1,1 +1,19 @@
+function clockWork(){
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+	var myArray = ['A', 'B', 'C', 'D', 'E', 'F'];
+	function arrayLoop(){ for(i = 0; i < myArray.length; i++){
+		return myArray[i];}
+	}
 
+	if(h <= 9){h = arrayLoop() + h};
+	if(m <= 9){m = arrayLoop() + m};
+	if(s <= 9){s = arrayLoop() + s};
+	clockColor = "#" + h + m + s; 
+	$('html').css("background-color", clockColor)
+	$('h1').text(clockColor);
+	setInterval(clockWork, 1000);
+};
+clockWork();
