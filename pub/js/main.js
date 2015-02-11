52efceca945f9e9b247436ed7f7bebdc3b9af4a3
Taylor Foster <tagfoster90@gmail.com>
2015-02-11 12:07:59 -0500

    chai...i guess

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,34 +1,7 @@
-/**
- * @constructor Board
- *
- * @method move(start, end) -- move `Piece` from `start` to `end`
- * @param String start position, e.g. "a7"
- * @param String end position, e.g. "a5"
- *
- * @method capture(position) -- capture `Piece` at `position`
- * @param String position of captured `Piece`, e.g. "d8"
- *
- * @method castle(rook) -- perform a castle maneuver
- * @param Boolean rook position to castle from
- * @uses move on `Rook` and `King` in one turn
- *
- * @method pieceAt(position) -- retrieve `Piece` at `position` or `null` if empty
- * @param String position to inspect, e.g. "a2", "c5"
- * @returns Piece|null if {row, col} is empty
- *
- * @method toString() -- simple representation of Board for debugging
- *
- * @method toArray() -- probably unnecssary
- */
-function Board(){
-  // Constructor
+var assert = chai.assert;
+var vm = new Vue({/* opttions */});
 
-}
-
-Board.prototype = {
-	move: function(start, end){
-		return this.start;
-		return this.end;
-	},
-	
-} // END Board.prototype
+assert.isDefined(vm.$el);
+console.assert(vm.$el);
+console.log(vm.$el);
+console.log(vm.$data);
