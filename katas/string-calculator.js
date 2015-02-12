3b991e8573c5e0d1c5cb9666ef56cd80101fcca1
Taylor Foster <tagfoster90@gmail.com>
2015-02-12 17:19:49 -0500

    more tests for string calc

--- katas/string-calculator.js
+++ katas/string-calculator.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var _ = require('lodash');
 
 var numbers = {
 	one: 1,
@@ -10,7 +11,17 @@ var numbers = {
 	seven: 7,
 	eight: 8,
 	nine: 9,
-	ten: 10
+	ten: 10,
+	eleven: 11,
+	twelve: 12,
+	thirteen: 13,
+	fourteen: 14, 
+	fifteen: 15,
+	sixteen: 16,
+	seventeen: 17,
+	eighteen: 18,
+	nineteen: 19,
+	twenty: 20
 }
 
 
@@ -26,6 +37,14 @@ it('should add "two" and "two" and equal number 4', function(){
 	assert.equal(plus("two", "two"), 4);
 });
 
-it('should add "three" and "three" and equal number 2', function(){
+it('should add "three" and "three" and equal number 6', function(){
 	assert.equal(plus("three", "three"), 6);
+});
+
+it('should add "four" and "four" and equal number 8', function(){
+	assert.equal(plus("four", "four"), 8);
+});
+
+it('should add "five" and "five" and equal number 10', function(){
+	assert.equal(plus("five", "five"), 10);
 });
