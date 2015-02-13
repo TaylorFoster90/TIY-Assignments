08c63cb2dd4839ec1138a809f4cae6588120a494
Taylor Foster <tagfoster90@gmail.com>
2015-02-13 07:45:48 -0500

    getting avg for gymnastics

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -5,15 +5,29 @@ var _ = require('lodash');
 var pushEvents = events.filter(function(item){
 	return item.type == 'PushEvent'
 });
+var eventDates = _.map(events, 'created_at')
+var pushDates = _.map(pushEvents, 'created_at')
 
-var pushDates = _.map(pushEvents, 'created_at');
+function getAvg () {
+	//pulls first date and last date so I can see how many days are logged into this json
+	var mostRecent =  _.first(eventDates).substring(0,10);
+	var leastRecent = _.last(eventDates).substring(0,10);
+	//looks like it took place over 3 days
+	return (pushEvents.length / 3); 
 
-console.log(pushDates)
+}
 
 
 function answer(){
 	return {
-		total: events.length
+		total: events.length,
+		PushEvent: {
+			total: pushEvents.length,
+			perDay: getAvg()
+		}
 	}
 }
 
+console.log(answer().PushEvent.perDay);
+console.log(eventDates)
+console.log(getAvg())
