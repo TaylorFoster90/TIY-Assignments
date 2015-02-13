97088f1307c4826ade8bf559fa57692017a9935e
Taylor Foster <tagfoster90@gmail.com>
2015-02-13 13:30:47 -0500

    total refactor, face lift, all funtion lookups

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -9,38 +9,54 @@ var pushEvents = events.filter(function(item){
 var otherEvents = events.filter(function(item){
 	return item.type != 'PushEvent'
 });
-var eventDates = _.map(events, 'created_at')
-var pushDates = _.map(pushEvents, 'created_at')
-//I made this more complicated than it needed to be but hey, it works 
-function getAvg () {
-	//pulls first date and last date so I can see how many days are logged into this json
-	var mostRecent =  _.first(eventDates).substring(0,10);
-	var leastRecent = _.last(eventDates).substring(0,10);
-	//lets get the difference between those
-	var getDiff = mostRecent.localeCompare(leastRecent);
-	it('should have a difference of 2, which is technically 3 days total', function(){
-		getAvg();
-		assert.equal(getDiff,2);
-	})
-
-	it('should show the average number of pushEvents per day is 5', function(){
-		assert.equal(pushEvents.length / (getDiff + 1), 5);
-	})
-	//the difference is 2 so because date math is wierd it's technically a span of 3 days
-	return pushEvents.length / (getDiff + 1)
+var createEvent = events.filter(function(item){
+	return item.type == 'CreateEvent'
+});
+var issueEvent = events.filter(function(item){
+	return item.type == 'IssuesEvent'
+});
+var deleteEvent = events.filter(function(item){
+	return item.type == 'DeleteEvent'
+});
+var prEvent = events.filter(function(item){
+	return item.type == 'PullRequestEvent'
+});
+function getDate(a){ 
+	return _.map(a, 'created_at')
+}
+function getDiff(a,b){
+	return _.first(a).substring(0.10).localeCompare(_.last(b).substring(0.10)) + 1;
+}
+function avg(item) {
+	var diff = getDiff(getDate(events),getDate(events));
+	return item.length / diff
 
 }
-
 
 function answer(){
 	return {
 		total: events.length,
 		PushEvent: {
 			total: pushEvents.length,
-			perDay: getAvg()
+			perDay: avg(pushEvents)
 		},
-		other: {
-			total: otherEvents.length
+		CreateEvent: {
+			total: createEvent.length,
+			perDay: avg(createEvent)
+		},
+		IssueEvent: {
+			total: issueEvent.length,
+			perDay: avg(issueEvent)
+		},
+		DeleteEvent: {
+			total: deleteEvent.length,
+			perDay: avg(deleteEvent)
+		},
+		PullRequestEvent: {
+			total: prEvent.length,
+			perDay: avg(prEvent)
 		}
 }
 }
+console.log(answer());
+
