384e3cb256e7cb9469ad34c514f2c210d513a3ff
Taylor Foster <tagfoster90@gmail.com>
2015-02-15 12:27:01 -0500

    still working on wiriting a loop to apply ids to the chessboard

--- chessboard/js/main.js
+++ chessboard/js/main.js
@@ -1,12 +1,17 @@
 
+var numbers = [1,2,3,4,5,6,7,8];
+var letter = ["a","b","c","d","e","f","g","h"];
+
+for (var i = 0; i < letter.length ;i++){
+	var list = letter[i];
+	console.log(list)
+}
+
 var vm = new Vue({
 	el: '#chessboard',
 	data: {
-		message: 'hello from Vue'
+		message: 'hello from Vue',
 	},
-	methods: {
-		piece: $(document).ready( function(){
-			$('td').html('hello world!')
-		})
-	}
+
 });
+
