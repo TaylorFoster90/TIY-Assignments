c809594bcb0181dd1fed422f99a7016a493d642a
Taylor Foster <tagfoster90@gmail.com>
2015-02-15 10:44:58 -0500

    got some things to print in the ceels using vue and query

--- chessboard/js/main.js
+++ chessboard/js/main.js
@@ -3,5 +3,10 @@ var vm = new Vue({
 	el: '#chessboard',
 	data: {
 		message: 'hello from Vue'
+	},
+	methods: {
+		piece: $(document).ready( function(){
+			$('td').html('hello world!')
+		})
 	}
 });
