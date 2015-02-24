262539e56220a7285eae22aeb3f42600b67e7fcb
Taylor Foster <tagfoster90@gmail.com>
2015-02-23 23:30:44 -0500

    faltlanders level 2

--- pub/js/main.js
+++ pub/js/main.js
@@ -3,10 +3,19 @@ var app = angular.module('store', [ ]);
 app.controller('StoreController', function(){
 	this.products = gems;
 });
+app.controller("PanelController", function(){
+	this.tab = 1;
+	this.selectTab = function (setTab) {
+		this.tab = setTab;
+	}
+	this.isSelected = function(checkTab) {
+		return this.tab === checkTab;
+	};
+})
 var gems = [
 {
 	name: 'Dodecahedron',
-	price: 2.95,
+	price: 2,
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 	canPurchase: true,
 },
